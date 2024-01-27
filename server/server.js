84ecab459528dc924b30b7006f6b5a00c6a9318b
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2024-01-27 22:28:55 +0600

    initialized project and db connection

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+export const app = express();
+dotenv.config();
+
+//Middlewears
+app.use(express.json());
+app.use(cors());
+const api = process.env.API;
+
+// Database Connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err.message));
+
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port ${process.env.PORT}`)
+);
